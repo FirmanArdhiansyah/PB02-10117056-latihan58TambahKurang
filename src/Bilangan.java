ce16b7d24b9702717c686ca339336cb62161cdc2
FirmanArdhiansyah <firmanardhi14@gmail.com>
2018-10-29 16:18:36 +0700

    Latihan58

---
+++ src/Bilangan.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Nama : Firman Ardhiansyah
+ *         Kelas : IF-2 (PB02)
+ *         NIM : 10117056
+ * Deskripsi Program : Program untuk menmbahkan dan mencari selisih bilangan
+ */
+public class Bilangan {
+    private int x;
+    private int y;
+    
+    public Bilangan() {
+        this.x = 3;
+        this.y = 4;
+    
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+    
+}
